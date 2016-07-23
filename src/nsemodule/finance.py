ab2248c8c05a5e17b3894441e2b5a86145eac023
Raj Patel <raj.patel@iref.com>
2016-07-23 16:01:23 +0530

    Consolidated fin params added

--- src/nsemodule/finance.py
+++ src/nsemodule/finance.py
@@ -4,6 +4,7 @@ import config
 import sys
 import json
 import six
+import code
 import re
 if six.PY2:
     from urlparse import urlparse
@@ -47,38 +48,49 @@ class FinanceData():
         result[-1].append(thetext)
     return result
 
-  def get_finance_params(self, symbol):
+  def get_finance_params(self, symbol, companyName = None):
     fetch_url = self.generate_fin_data_url(symbol)
     status = fetch_url['status']
+    if status != 200:
+      fetch_url = self.generate_fin_data_url(symbol, companyName)
     if status == 200:
       fin_url = fetch_url['response']['mc_url']
       r = requests.get(fin_url)
+      status = r.status_code
       if r.status_code == 200:
-        try:
-          target_div = BeautifulSoup(r.text,"html.parser").find('div',attrs = {"id":"mktdet_1","name":"mktdet_1"})
-          values_div = target_div.find_all('div',attrs={"class":"PA7 brdb"})
-          params = []
-          for div in values_div:
-            key_div = div.find('div',attrs={"class":"FL gL_10 UC"})
-            value_div = div.find('div',attrs={"class":"FR gD_12"})
-            try:
-              key = key_div.contents[0].lower()
-            except:
-              key = None
-            try:
-              value = value_div.contents[0].replace(',','')
-            except:
-              value = None 
-            params.append([key,value])
-          response = self.map_fin_params_data(params)
-        except:
-          print("Unexpected error:", sys.exc_info()[0])
-          status = 422
-      else:
-        status = r.status_code
+        standalone = self.parse_data_mc_company_page(r.text, "mktdet_1", "mktdet_1")
+        consolidated = self.parse_data_mc_company_page(r.text, "mktdet_2", "mktdet_2")
+        response = {"standalone": standalone, "consolidated": consolidated }    
+    else:
+      print("Unable to generate URL",symbol,companyName)
     if status != 200:
       response = config.error_msg[str(status)]   
     return {"response": response, "status": status}
+
+  def parse_data_mc_company_page(self, htmltext, div_id, div_name):
+    target_div = BeautifulSoup(htmltext,"html.parser").find('div',attrs = {"id":div_id,"name":div_name})
+    response = None
+    if target_div != None:
+      values_div = target_div.find_all('div',attrs={"class":"PA7 brdb"})
+      params = []
+      if values_div != None:
+        for div in values_div:
+          key_div = div.find('div',attrs={"class":"FL gL_10 UC"})
+          value_div = div.find('div',attrs={"class":"FR gD_12"})
+          try:
+            key = key_div.contents[0].lower()
+          except:
+            if len(key_div.contents) == 2:
+              key = key_div.contents[1].lower()
+            else:
+              key = None
+          try:
+            value = value_div.contents[0].replace(',','')
+          except:
+            value = None 
+          params.append([key,value])
+      response = self.map_fin_params_data(params)
+    return response
 
   def map_fin_params_data(self,params):
     fparams = { 'market_cap' : None, 'p_by_e' : None, 'book_value' : None,
@@ -86,70 +98,75 @@ class FinanceData():
       'price_by_book' : None, 'div_yield_perc' : None, 'put_by_call' : None}
     for x in xrange(len(params)):
       if re.match('market\scap[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1]):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1]):
           fparams['market_cap'] = float(params[x][1])
       elif re.match('p\/e[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1]):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1]):
           fparams['p_by_e'] = float(params[x][1])
       elif re.match('book\svalue[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1]):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1]):
           fparams['book_value'] = float(params[x][1])
       elif re.match('div\s\(\%\)[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1].replace('%','')):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1].replace('%','')):
           fparams['div_perc'] = float(params[x][1].replace('%',''))
       elif re.match('industry\sp\/e[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1]):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1]):
           fparams['industry_p_by_e'] = float(params[x][1])
       elif re.match('eps[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1]):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1]):
           fparams['eps'] = float(params[x][1])
       elif re.match('price\/book[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1]):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1]):
           fparams['price_by_book'] = float(params[x][1])
       elif re.match('div\syield[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1].replace('%','')):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1].replace('%','')):
           fparams['div_yield_perc'] = float(params[x][1].replace('%',''))
       elif re.match('p\/c[\w]*',params[x][0]) != None:
-        if re.match('[\d.]+',params[x][1]):
+        if params[x][1] != None and re.match('[\d.]+',params[x][1]):
           fparams['put_by_call'] = float(params[x][1])
       else:
         pass
     return fparams 
 
-  def generate_fin_data_url(self,symbol):
-    autosuggest_url = config.mc['autosuggest'].format(symbol)
-    try:
-      r_suggestion = requests.get(autosuggest_url)
-      if r_suggestion.status_code == 200:
-        suggest_dict = json.loads(r_suggestion.text[1:-1])
-        if suggest_dict[0][config.mc['suggest_parser']['info']] != config.mc['suggest_parser']['no_suggest'] :
-          # Company which is listed on BSE & NSE will have format of (name,nse_symbol,bse_code)
-          # For those which are listed only on NSE, the format will be (name <some code>,nse_symbol)
-          found = None
-          for s in suggest_dict:
-            info_list = s[config.mc['suggest_parser']['info']].split(",")
-            # Case BSE & NSE listed companies - list will have 3 entities
-            # Case NSE listed companies - list will have 2 entities
-            if len(info_list) in [ 2, 3 ] and BeautifulSoup(info_list[1],"html.parser").text.strip().upper() == symbol.upper():
-              url = urlparse(s[config.mc['suggest_parser']['url']])
-              try:
-                path_list = url.path.split('/')
-                response = {"symbol": path_list[-1],"name":path_list[-2],'url': config.mc['balancesheet'].format(path_list[-2],path_list[-1]),'mc_url': s[config.mc['suggest_parser']['url']]}
-                status = 200
-                found = True
-                break
-              except:
-                status = 422
-          
-          if found == None:
-            status = 204
-        else:
+  def generate_fin_data_url(self,symbol,companyName = None):
+    if companyName is not None:
+      company_name = companyName.replace('&','and').lower().replace('limited','').replace('ltd','')
+      autosuggest_url = config.mc['autosuggest'].format(company_name)
+    else:
+      autosuggest_url = config.mc['autosuggest'].format(symbol)
+    
+    r_suggestion = requests.get(autosuggest_url)
+    if r_suggestion.status_code == 200 and len(r_suggestion.text) > 1:
+      suggest_dict = json.loads(r_suggestion.text[1:-1])
+      if suggest_dict[0][config.mc['suggest_parser']['info']] != config.mc['suggest_parser']['no_suggest'] :
+        # Company which is listed on BSE & NSE will have format of (name,nse_symbol,bse_code)
+        # For those which are listed only on NSE, the format will be (name <some code>,nse_symbol)
+        found = None
+        for s in suggest_dict:
+          info_list = s[config.mc['suggest_parser']['info']].split(",")
+          # Case BSE & NSE listed companies - list will have 3 entities
+          # Case NSE listed companies - list will have 2 entities
+          if len(info_list) in [ 2, 3 ] and BeautifulSoup(info_list[1],"html.parser").text.strip().upper() == symbol.upper():
+            url = urlparse(s[config.mc['suggest_parser']['url']])
+            try:
+              path_list = url.path.split('/')
+              response = {"symbol": path_list[-1],"name":path_list[-2],'url': config.mc['balancesheet'].format(path_list[-2],path_list[-1]),'mc_url': s[config.mc['suggest_parser']['url']]}
+              status = 200
+              found = True
+              break
+            except:
+              print(" Unable to parse url for ", symbol, companyName)
+              status = 422
+        
+        if found == None:
+          print("Not able to found",symbol)
           status = 204
       else:
-        status = 404
-    except:
-      print("Unexpected error:", sys.exc_info()[0])
-      status = 422
+        print(" Unexpeceted format received for ", symbol, companyName)
+        status = 204
+    else:
+      print("Unable to get response from Autosuggest URL for ", symbol, companyName)
+      status = 404
     if status != 200:
         response = config.error_msg[str(status)]
     return {'response': response,'status': status }
